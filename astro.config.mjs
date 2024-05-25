0fda844fe27c9de7f0db21def1505304743ce361
bbauti <bautistaigarzabal11@gmail.com>
2024-05-24 23:42:19 -0300

    feat: continue footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
+import lottie from "astro-integration-lottie";
 
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://test.bbauti.ar',
-  integrations: [tailwind(), icon(), sitemap(), robotsTxt()]
+  integrations: [tailwind(), icon(), sitemap(), robotsTxt(), lottie()]
 });
