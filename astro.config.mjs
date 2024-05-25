c9bd7b30c4e760add53b10d7fe8d52c13a38675e
bbauti <bautistaigarzabal11@gmail.com>
2024-05-25 14:54:38 -0300

    fix: remove cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 import lottie from "astro-integration-lottie";
-import robotsTxt from "astro-robots-txt";
 
-import cloudflare from "@astrojs/cloudflare";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://test.bbauti.ar',
-  integrations: [tailwind(), icon(), sitemap(), robotsTxt(), lottie()],
-  output: "server",
-  adapter: cloudflare()
+  integrations: [tailwind(), icon(), sitemap(), robotsTxt(), lottie()]
 });
