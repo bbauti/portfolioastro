baed80f98c05ed7db6b0b8966510218f4c397e11
bbauti <bautistaigarzabal11@gmail.com>
2024-05-25 14:52:32 -0300

    feat: add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 import lottie from "astro-integration-lottie";
-
 import robotsTxt from "astro-robots-txt";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://test.bbauti.ar',
-  integrations: [tailwind(), icon(), sitemap(), robotsTxt(), lottie()]
+  integrations: [tailwind(), icon(), sitemap(), robotsTxt(), lottie()],
+  output: "server",
+  adapter: cloudflare()
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="astro/client" />
 /// <reference types="astro-integration-lottie/env" />
+type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+type ENV = {
+  lastlocation: KVNamespace;
+};
+
+type Runtime = import("@astrojs/cloudflare").Runtime<ENV>;
+declare namespace App {
+  interface Locals extends Runtime {}
+}
