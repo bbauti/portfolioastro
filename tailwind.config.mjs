08f123f60d807d73f4ad5979cca1581e61d5a644
Bautista Igarzabal <bautista.igarzabal@avoristravel.com>
2024-05-31 10:57:21 -0300

    fix: wrong fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,10 +9,6 @@ export default {
         'project-outline': 'rgba(0, 0, 0, 0.05) 0px -3px 0px 0px inset, rgb(255, 255, 255) 0px 0px 0px 2px inset, rgba(0, 0, 0, 0.06) 0px 4px 2px 0px inset, rgba(0, 0, 0, 0.04) 0px 0px 24px 4px inset, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px',
         'project-outline-dark': 'rgba(255, 255, 255, 0.1) 0px -3px 0px 0px inset, rgba(0, 0, 0, 0.5) 0px 0px 0px 2px inset, rgba(255, 255, 255, 0.15) 0px 4px 2px 0px inset, rgba(255, 255, 255, 0.1) 0px 0px 24px 4px inset, rgba(255, 255, 255, 0.2) 0px 1px 3px 0px',
       },
-      fontFamily: {
-        'sans': ['"Inter"', ...defaultTheme.fontFamily.sans],
-        'mono': ['"Geist"', ...defaultTheme.fontFamily.mono],
-      },
     },
   },
   plugins: [],
