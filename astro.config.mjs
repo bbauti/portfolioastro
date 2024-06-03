e3d98b89ce6a8eb0f253bd4ceb8cf2329afacaf1
bbauti <bautistaigarzabal11@gmail.com>
2024-06-03 11:15:34 -0300

    feat: init translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,21 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 import lottie from "astro-integration-lottie";
+import paraglide from "@inlang/paraglide-astro"
 
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://test.bbauti.ar',
-  integrations: [tailwind(), icon(), sitemap(), robotsTxt(), lottie()],
+  i18n: {
+		locales: ['en', 'es'],
+		defaultLocale: "en",
+	},
+
+  site: 'https://bbauti.ar',
+  integrations: [tailwind(), icon(), sitemap(), robotsTxt(), lottie(), paraglide({
+    project: "./project.inlang",
+    outdir: "./src/paraglide",
+  })],
   renderers: [],
 });
