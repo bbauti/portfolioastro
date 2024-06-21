212290e1aa35659a37cf59c45a3b9fdc063075ec
bbauti <bautistaigarzabal11@gmail.com>
2024-06-21 17:05:40 -0300

    revert: undo translations merge due to issues with translations
    This reverts commit 938b24a2f270a6d986f03dbd7529afa700bbc6d6, reversing
    changes made to 55cfa9594c5599dff85de4e8be45388bec22f49f.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 import lottie from "astro-integration-lottie";
-import paraglide from "@inlang/paraglide-astro"
 
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  i18n: {
-		locales: ['en', 'es'],
-		defaultLocale: "en",
-	},
-
-  site: 'https://bbauti.ar',
-  integrations: [tailwind(), icon(), sitemap(), robotsTxt(), lottie(), paraglide({
-    project: "./project.inlang",
-    outdir: "./src/paraglide",
-  })],
+  site: 'https://test.bbauti.ar',
+  integrations: [tailwind(), icon(), sitemap(), robotsTxt(), lottie()],
   renderers: [],
 });
